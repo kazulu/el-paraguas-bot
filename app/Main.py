fe26b6084e5224dbe12b947f5741a5cbc92afb22
kazulu <antonioacunadev@gmail.com>
2020-07-17 00:22:57 +0200

    Now generate a welcome with sentences from a list

--- app/Main.py
+++ app/Main.py
@@ -1,9 +1,26 @@
-import re
+import re, random
 from telegram.ext import Updater, MessageHandler, Filters
-from config import token
+from app.config import token
 
 pepe_sticker_id = "CAACAgQAAxkBAAOyXw4dNEA3mbtu7tIXClE3_PGRKHkAAkEBAAKoISEGr2bGG23uS4saBA"
 pepe_sticker_unique_id = "AgADQQEAAqghIQY"
+
+welcome_sentences = [
+    "del Imperio que crea software",
+    "de Jesucristo programador",
+    "de Java",
+    "de Python",
+    "de Román el europeo",
+    "de Moldavia",
+    "de la ingeniera rusa",
+    "de Cecilio",
+    "de Ayuso",
+    "de Leetcode",
+    "del Clean Code",
+    "de las apps basura del Play Store",
+    "de España",
+    "del Espíritu Santo"
+]
 
 def reply_with_sticker(update, context):
     update.message.reply_sticker(sticker = pepe_sticker_id)
@@ -13,13 +30,25 @@ def reply_with_text(update, context):
         update.message.reply_text("Abriendo paraguas")
 
 def welcome_message(update, context):
-    update.message.reply_text("Por la gloria del Imperio, bienvenido")
+    update.message.reply_text(create_welcome_message())
+
+def create_welcome_message():
+    sentences = []
+    while len(sentences) < 3:
+        random_sentence = random.choice(welcome_sentences)
+        if random_sentence not in sentences:
+            sentences.append(random_sentence)
+    message = ", ".join(sentences)
+    replace = message.rfind(",")
+    new_message = message[:replace] + " y" + message[replace + 1:]
+
+    return "Por la gloria " + new_message + " yo te bendigo y te doy la bienvenida."
 
 def main():
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(MessageHandler(Filters.regex(re.compile(r'paraguas', re.IGNORECASE)), reply_with_sticker))
+    dispatcher.add_handler(MessageHandler(Filters.regex(re.compile(r'abr(e|o|iendo) paraguas', re.IGNORECASE)), reply_with_sticker))
     dispatcher.add_handler(MessageHandler(Filters.sticker, reply_with_text))
     dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcome_message))
 
