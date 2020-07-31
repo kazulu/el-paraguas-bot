b6bc702e6a604807737dc9f125b107d8a9df8219
Natán Castro <hello@natan.dev>
2020-07-31 23:39:20 +0200

    Added mongodb database for the welcome sentences. Added /show command to show all the welcome sentences in the DB. Added /add command to add a new welcome sentences to the DB. Added /remove command to remove a current welcome sentences from the DB.

--- app/config.bak.py
+++ app/config.bak.py
@@ -1,1 +1,9 @@
-token = "your token here"
+# Telegram token
+token = 'your token here'
+
+# MongoDB
+mongodb_database = 'paraguasBot'
+mongodb_collection = 'welcomeSentences'
+
+# Users who can add welcome sentences
+welcome_users = ['@natan', '@kazulu']
