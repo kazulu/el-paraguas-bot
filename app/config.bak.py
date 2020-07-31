f070cea30c259bd236a9a117e85b37f763ada9eb
Natán Castro <hello@natan.dev>
2020-07-31 23:46:52 +0200

    Changed some variables.

--- app/config.bak.py
+++ app/config.bak.py
@@ -4,6 +4,8 @@ token = 'your token here'
 # MongoDB
 mongodb_database = 'paraguasBot'
 mongodb_collection = 'welcomeSentences'
+mongodb_host = 'localhost'
+mongodb_port = 27017
 
 # Users who can add welcome sentences
 welcome_users = ['@natan', '@kazulu']
